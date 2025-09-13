6f9e21489266441d92072356773e0369dc8c1e34
alexmam80 <166691364+alexmam80@users.noreply.github.com>
2025-09-13 10:54:02 +0300

    Update CtaBanner.jsx

--- client/src/components/CtaBanner.jsx
+++ client/src/components/CtaBanner.jsx
@@ -10,7 +10,7 @@ export default function CtaBanner(){
         <h2>{t('cta.title')}</h2>
         <p>{t('cta.text')}</p>
         <div className="logo-center">
-			<a href="https://smartgreengroup.md"><img src="/public/Whitelogo.png" alt="SmartGreenGroup - Sistemul colectiv Asociația Obștească" /></a>
+			<a href="https://smartgreengroup.md"><img src="/SGG/Whitelogo.png" alt="SmartGreenGroup - Sistemul colectiv Asociația Obștească" /></a>
 		</div>
         <div className="cta-buttons">
             <a className="btn outline" href="/contacte">{t('cta.btn')}</a>
